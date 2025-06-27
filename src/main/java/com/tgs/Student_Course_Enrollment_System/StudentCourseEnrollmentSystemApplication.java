e663fb995768c412bba463c4b611a64563528b4a
Raghava156 <raghavanallabothula156@gmail.com>
2025-06-27 14:28:15 +0530

    setup

---
+++ src/main/java/com/tgs/Student_Course_Enrollment_System/StudentCourseEnrollmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tgs.Student_Course_Enrollment_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCourseEnrollmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCourseEnrollmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgs/Student_Course_Enrollment_System/StudentCourseEnrollmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgs.Student_Course_Enrollment_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseEnrollmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
